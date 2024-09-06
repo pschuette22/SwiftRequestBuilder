960c384e7089184bc1b1bf17dc859f654d811738
Pete <pschuette22@gmail.com>
2024-09-06 14:08:25 -0500

    ship test helpers

--- Package.swift
+++ Package.swift
@@ -12,7 +12,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftRequestBuilder",
-            targets: ["SwiftRequestBuilder"])
+            targets: ["SwiftRequestBuilder"]
+        ),
+        .library(
+            name: "SwiftRequestBuilderTestHelpers", 
+            targets: ["SwiftRequestBuilderTestHelpers"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
