5302a7ece0abbcde93480c6d7220627d0d126ace
Pete <pschuette22@gmail.com>
2024-09-06 14:12:09 -0500

    [CHORE] TestHelpers  (#4)
    * ship test helpers
    
    * remove empty schema

--- Package.swift
+++ Package.swift
@@ -12,11 +12,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftRequestBuilder",
-            targets: ["SwiftRequestBuilder"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["SwiftRequestBuilder"]
+        ),
+        .library(
+            name: "SwiftRequestBuilderTestHelpers", 
+            targets: ["SwiftRequestBuilderTestHelpers"]
+        )
     ],
     targets: [
         .target(
